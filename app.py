3f01823235662af41fa871622077059d2b081c03
AnTran210 <21520553@gm.uit.edu.vn>
2024-06-03 00:01:54 +0700

    Add chart for frequency

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, request, render_template, url_for, redirect
+import pandas as pd
 
 app = Flask(__name__)
 
+def get_data(filename):
+    df = pd.read_json(f"test-data/{filename}.json", lines=True)
+    data_dict = dict(sorted(df['num_watch_vid'].value_counts().to_dict().items()))
+    data_keys = list(data_dict.keys())
+    data_values = list(data_dict.values())
+    return data_keys, data_values
+    
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    data_keys, data_values = get_data("output_1")
+    return render_template("index.html", data_keys=data_keys, data_values=data_values)
 
 
 if __name__ == "__main__":
