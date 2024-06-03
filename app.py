e243d2d0955aec4c5a745a887000d0885ba70b77
AnTran210 <21520553@gm.uit.edu.vn>
2024-06-03 18:28:52 +0700

    Group data to show in chart

--- app.py
+++ app.py
@@ -3,11 +3,39 @@ import pandas as pd
 
 app = Flask(__name__)
 
+
+def group_data(data_dict):
+    grouped_data = {
+        '0-10': 0,
+        '11-50': 0,
+        '51-100': 0,
+        '101-150': 0,
+        '151-200': 0,
+        '>200': 0
+    }
+
+    for key, value in data_dict.items():
+        if key <= 10:
+            grouped_data['0-10'] += value
+        elif 11 <= key <= 50:
+            grouped_data['11-50'] += value
+        elif 51 <= key <= 100:
+            grouped_data['51-100'] += value
+        elif 101 <= key <= 150:
+            grouped_data['101-150'] += value
+        elif 151 <= key <= 200:
+            grouped_data['151-200'] += value
+        else:
+            grouped_data['>200'] += value
+
+    return grouped_data
+
 def get_data(filename):
     df = pd.read_json(f"test-data/{filename}.json", lines=True)
     data_dict = dict(sorted(df['num_watch_vid'].value_counts().to_dict().items()))
-    data_keys = list(data_dict.keys())
-    data_values = list(data_dict.values())
+    grouped_data = group_data(data_dict)
+    data_keys = list(grouped_data.keys())
+    data_values = list(grouped_data.values())
     return data_keys, data_values
     
 
